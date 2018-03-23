634caa4b84d8c8644e9f3ae40eddc65f1a05ae7e
aangelot <anthony.angelot@gmail.com>
2018-03-23 17:08:13 +0100

    Mix cutting and analysing functions

--- speechtotext.py
+++ speechtotext.py
@@ -3,13 +3,16 @@ import os
 import wave
 import contextlib
 from pydub import AudioSegment
+from cutter import cutter
+import numpy as np 
 
 # Imports the Google Cloud client library
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
-def speechtotext(file_path,file):
+def speechtotext(file,file_path):
+    '''speech to text for a file of less than a minute'''
     # Instantiates a client
     client = speech.SpeechClient()
 
@@ -25,7 +28,7 @@ def speechtotext(file_path,file):
     if nchannels > 1:
         sound = AudioSegment.from_wav(file_name)
         sound = sound.set_channels(1)
-        output_path = file_path + file[:-4] + '_mono.wav'
+        output_path = file_path + '/' + file[:-4] + '_mono.wav'
         sound.export(output_path, format="wav")
         file_name = output_path
 
@@ -46,4 +49,21 @@ def speechtotext(file_path,file):
     response = client.recognize(config, audio)
     for result in response.results:
         return('{}'.format(result.alternatives[0].transcript))
+
+def analyse(file_name,file_path=os.getcwd(),duration=3):
+    '''from an audio return the files of the selected duration,
+    analyse if each file is music or voice and give the text'''
+    cutter(file_name,duration)
+    os.chdir(os.getcwd()+'/'+file_name[:-4]+'_files/') 
+    files = os.listdir(os.getcwd()) #get a list of all the audio
+    files.sort()
+    output = []
+    for file in files:
+        text = speechtotext(file,os.getcwd())
+        print(text)
+        output.append((file,text))
+    return output
+
+print(analyse('example.wav',duration=20))
+
 
