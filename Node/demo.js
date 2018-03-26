85033c98b836442dc37dda8f3c7bd327ceebd8f8
Dudley smith <vj.dudley.smith@gmail.com>
2018-03-26 13:20:59 +0200

    Commenting socket

--- Node/demo.js
+++ Node/demo.js
@@ -5,12 +5,12 @@ var express = require('express');
 var app = express();
 
 // Socket section
-var socket = require('socket.io');
-var io = socket(server);
-io.sockets.on('connection', newConnection);
-function newConnection(socket){
-    console.log("New connection: " + socket.id);
-}
+// var socket = require('socket.io');
+// var io = socket(server);
+// io.sockets.on('connection', newConnection);
+// function newConnection(socket){
+//     console.log("New connection: " + socket.id);
+// }
 
 app.use('/src', express.static(__dirname + '/src'));
 
