4821c77ec8db4265f5d0cedf477fbb2043c00619
Kevin Vennitti <kevin.vennitti@gmail.com>
2018-03-26 13:51:55 +0200

    Avec la pièce jointe, c'est mieux

--- Node/demo.js
+++ Node/demo.js
@@ -1,33 +1,64 @@
-var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var BinaryServer = require('binaryjs').BinaryServer;
+var wav = require('wav');
+
+var port = 3700;
+var outFile = 'demo.wav';
+
+binaryServer = BinaryServer({port: 9001});
+
+binaryServer.on('connection', function(client) {
+  console.log('new connection');
+
+  var fileWriter = new wav.FileWriter(outFile, {
+    channels: 1,
+    sampleRate: 48000,
+    bitDepth: 16
+  });
+
+  client.on('stream', function(stream, meta) {
+    console.log('new stream');
+    stream.pipe(fileWriter);
+
+    stream.on('end', function() {
+      fileWriter.end();
+      console.log('wrote to file ' + outFile);
+    });
+  });
+});
 
 // Socket section
-// var socket = require('socket.io');
-// var io = socket(server);
-// io.sockets.on('connection', newConnection);
-// function newConnection(socket){
-//     console.log("New connection: " + socket.id);
-// }
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+    console.log("New connection: " + socket.id);
+}
 
 app.use('/src', express.static(__dirname + '/src'));
 
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/src/home.html');
+    res.sendFile(__dirname + '/src/home.html');
 });
 
 app.get('/record', function(req, res) {
-    res.sendfile(__dirname + '/src/record.html');
+    res.sendFile(__dirname + '/src/record.html');
 });
 
 app.get('/analysis', function(req, res) {
-    res.sendfile(__dirname + '/src/analysis.html');
+    res.sendFile(__dirname + '/src/analysis.html');
 });
 
 app.get('/*', function(req, res) {
-    res.sendfile(__dirname + '/src/error404.html');
+    res.sendFile(__dirname + '/src/error404.html');
 });
 
-app.listen(3000);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
