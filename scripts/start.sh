2bfae3e922d5fc40c1643bb8e09661c7e55bd16c
DudleySmith <vj.dudley.smith@gmail.com>
2018-04-06 16:49:21 +0200

    scripts de démarrage et d'arrêt des services du dispositif

---
+++ scripts/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WORK_DIR="/home/erasme/lasondia"
+PATH=$PATH:/usr/share/processing/processing-3.3.7
+export GOOGLE_APPLICATION_CREDENTIALS="/home/erasme/lasondia/Node/googlecredentials.json"
+
+# Démmarage de processing : gestion du switch lumière
+nohup processing-java  --sketch=$WORK_DIR/BoutonLumiere --run --present > /tmp/BoutonLumiere.log & 
+sleep 2
+
+# demarrage du serveur node
+cd $WORK_DIR/Node
+nohup node demo.js > /tmp/node.log &
+sleep 2
+
+# Lancer FireFox sur le bon lien
+firefox http://localhost:8000/
+
---
+++ scripts/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Arreter Firefox
+killall /usr/lib/firefox/firefox
+
+# arreter node
+killall node
+
+# arreter processing
+killall java
